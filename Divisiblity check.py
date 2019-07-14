8700fe1cbd723f4d84264b18058519967f56559b
zaidtab123 <zaid.tab123@gmail.com>
2019-07-14 13:02:03 +0530

    Initial Commit

--- Divisiblity check.py
+++
@@ -1,7 +0,0 @@
-x=int(input("Enter the first number:"))
-y=int(input("Enter the second number:"))
-if(x%y==0):
-    print("The number is divisible.")
-    
-else:
-    print("The number is not divisible.")
--- length_to_inch(if loop).py
+++
@@ -1,6 +0,0 @@
-x=float(input("Enter the length: "))
-if x<0:
-    print("The entry is invalid")
-else:
-    y=x/2.54
-    print("Answer is:",round(y,2),"inches.")
